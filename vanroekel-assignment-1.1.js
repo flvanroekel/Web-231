8055685ee83310486de594ad77416847779fa587
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-05-29 21:55:24 -0500

    Add files via upload

---
+++ vanroekel-assignment-1.1.js
@@ -0,0 +1,34 @@
+/*
+=============================================
+; Title: Assignment 1.1
+; Author: Faye Van Roekel
+; Date: 29 May 2019
+; Modified By: 
+; Description: This program has errors in a program
+; for my classmates to fix.
+;============================================
+*/
+
+/* Start Program */
+
+<script>
+alert('Welcome to the Error Website');
+</script>
+
+/*
+  Expected output:
+    Console out, Karen Kann, 560 First Street Circle, Carnes, IA, 51250
+*/
+
+// Declare Variables
+var name = 'Karen Kann";
+var address = 560 First Street Circle;
+var city = "Carnes";
+var state = "Iowa";
+var zip = 51250
+
+// Output
+console.output("name" + "address" + "city" + "state" + "zip");
+
+/*=============================================
+// End program*/
