5aebf4e3139964266bdbae062d867d8be303cd15
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-11 14:13:56 -0500

    vanroekel-exercise-3.1.js

---
+++ week-3/vanroekel-exercise-3.1.js
@@ -0,0 +1,32 @@
+const header = require('../Vanroekel-header.js')
+                       
+console.log(header.display("Faye", "Van Roekel", "Assignment 3.1"));
+
+/*
+============================================
+; Title:  Assignment 3.1 Control Statements
+; Author: Faye Van Roekel
+; Date:   11 June 2019
+; Description: Control Statements 3.1 Assignment with Errors
+;===========================================
+*/
+
+// start program
+
+if username == null  //if username is null or not defined,
+    username = Faye Van Roekel  //define it
+
+
+    var text = "";
+
+    var i = 11;
+while (i < 10) 
+  text += "<br>The number is " + i;
+  i++;
+}
+
+// Output
+console.output("username" + "The number is " + i);
+
+/*=============================================
+// End program*/
