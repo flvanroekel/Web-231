351d90c443e58274b92ba912582c7873c7faa5fc
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-11 16:12:20 -0500

    fixed title

---
+++ week-3/vanroekel-assignment-3.3.js
@@ -0,0 +1,24 @@
+const header = require('../Vanroekel-header.js')
+                       
+console.log(header.display("Faye", "Van Roekel", "Assignment 3.1"));
+
+/*
+============================================
+; Title:  Assignment 3.2 Pattern Matching Functions
+; Author: Faye Van Roekel
+; Date:   11 June 2019
+; Description: Pattern Matching Functions
+;===========================================
+*/
+
+// start program
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 3.3
+  Today's Date
+
+  // Expected output
+  The enter key was pressed
+*/
