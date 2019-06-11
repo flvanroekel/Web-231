1189080975bc143c83baf655b12a8f108de4b51e
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-11 16:30:03 -0500

    vanroekel-assignment-3.3.js

--- week-3/vanroekel-assignment-3.3.js
+++ week-3/vanroekel-assignment-3.3.js
@@ -1,13 +1,14 @@
 const header = require('../Vanroekel-header.js')
                        
-console.log(header.display("Faye", "Van Roekel", "Assignment 3.1"));
+console.log(header.display("Faye", "Van Roekel", "Assignment 3.3"));
 
 /*
 ============================================
-; Title:  Assignment 3.2 Pattern Matching Functions
+; Title:  Assignment 3.3 Control Statements
 ; Author: Faye Van Roekel
+; Modified by: 
 ; Date:   11 June 2019
-; Description: Pattern Matching Functions
+; Description: Control Statements
 ;===========================================
 */
 
@@ -22,3 +23,25 @@ console.log(header.display("Faye", "Van Roekel", "Assignment 3.1"));
   // Expected output
   The enter key was pressed
 */
+
+
+let eventKeyCode = 13
+
+
+switch (eventKeyCode) {
+    case 13:
+        console.log("The enter key was pressed.");
+        break;
+    case 16:
+        console.log("The shift key was pressed.");
+        break;
+    case 32:
+        console.log("The spacebar key was pressed.");
+        break;
+    case 8:
+        console.log("The backspace / delete key was pressed.");
+        break;
+    default:
+        console.log("Unrecognized key.");
+        break;
+}
