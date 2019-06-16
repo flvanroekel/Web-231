83d3299d4bc8b14310c9d33c7e79b0cf10a63a66
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-16 16:03:58 -0500

    lyman-errors-assignment-week3

---
+++ week-3/lyman-discussion-3.1-fixed-errors.js
@@ -0,0 +1,43 @@
+/*
+====================
+Title: Exercise 3.1
+Author: Ashleigh Lyman
+Date: 16 June 2019
+Modified by: Faye Van Roekel
+Description: Program using if statement with 2 errors
+;====================
+*/
+
+// Importing and outputting header file required for all JS assignments
+
+var header = require('./Lyman-header.js');
+console.log(header.display('Ashleigh', 'Lyman', 'Exercise 3.1 - Control Statements'));
+
+/*
+Expected outcome:
+Ashleigh Lyman
+Exercise 3.1 - Control Statements
+6/12/2019
+You have 3 items in your shopping cart
+*/
+
+//Start Program
+
+//Declare variable
+var b = 3;
+
+//Begin if statement, if variable does equal 1 then display message.
+if (a ==1);
+
+if (condition) {a==1)''
+    
+} else {
+    console.log('\n' + "You have " + b + " items in your shopping cart.");
+}
+ 
+//If else then display variable amount in message.
+elseif
+  console.log('\n' + "You have " + b + " items in your shopping cart.");
+
+
+//End Program
