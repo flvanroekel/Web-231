9111d3a13cbaf7bba55a5375751ed0ab42162ef8
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-18 19:22:09 -0500

    vanroekel-assignment-4.1.js

---
+++ week-4/vanroekel-assignment-4.1.js
@@ -0,0 +1,28 @@
+const header = require('../Vanroekel-header.js')
+                       
+console.log(header.display("Faye", "Van Roekel", "Assignment 4.1"));
+
+/*
+============================================
+; Title:  Assignment 4.1 Control Statements
+; Author: Faye Van Roekel
+; Date:   18 June 2019
+; Description: Arrays 4.1 Assignment with Errors
+;===========================================
+*/
+
+// start program
+
+var jewelry = [Ruby, Sapphire, Pearl];
+
+jewelry[3] = "Diamond";
+
+var person = ["Larry", "Lane", 45];
+person[4];
+
+console.log(jewelry[3]) //report out the Diamond
+console.log(person[4])  //report out the 4th element in the array
+
+
+/*=============================================
+// End program*/
