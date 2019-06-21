cfb202bba1d89cd3c2041dd35a64588bf2278358
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-21 14:39:47 -0500

    dockter-Errors-fixed-vanroekel-assignment-4.1.js

---
+++ week-4/dockter-Errors-fixed-vanroekel-assignment-4.1.js
@@ -0,0 +1,31 @@
+const header = require('../Vanroekel-header.js')
+                       
+console.log(header.display("Faye", "Van Roekel", "Assignment 4.1"));
+
+/*
+============================================
+; Title:  Discussion 4.1
+; Author: Brittany Dockter 
+; Date:   20 June 2019
+; Modified by: Faye Van Roekel
+; Description: A simple program using Arrays that contain two errors.
+;===========================================
+*/
+
+/* Expected Output
+Brittany Dockter
+Discussion 4.1
+Date: 06/20/2019
+My favorite food is chicken
+*/
+
+// Begin Program
+
+
+var meat = ["steak", "chicken", "pork"];
+meat["1"]
+
+
+// Output
+
+console.log("My favorite food is" , meat[1]);
