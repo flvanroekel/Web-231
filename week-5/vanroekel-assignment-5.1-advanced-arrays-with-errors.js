0ee2226a33b4f0e4b3c345981b9700df3d1cbe93
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-25 14:57:38 -0400

    vanroekel-assignment-5.1-advanced-arrays-with-errors.js

---
+++ week-5/vanroekel-assignment-5.1-advanced-arrays-with-errors.js
@@ -0,0 +1,40 @@
+const header = require('../Vanroekel-header.js')
+                       
+console.log(header.display("Faye", "Van Roekel", "Assignment 5.1"));
+
+/*
+============================================
+; Title:  Assignment 5.1 Advanced Arrays
+; Author: Faye Van Roekel
+; Date:   18 June 2019
+; Description: Arrays 5.1 Advanced Arrays with Errors
+;===========================================
+*/
+/*
+Expected Output:
+; [4,5,6,7]
+; [2, 8, 18, 32]
+*/
+
+// start program
+
+var numArray = [2, 3, 4, 5]
+var newnumberArray = numArray.map(numArray => {
+    return numItem + 2
+})
+
+
+var array1 = [1, 4, 9, 16];
+
+var map1 = array2.map(x => x * 2);
+
+
+
+
+
+
+console.log(newnumberArray);
+console.log(map1);
+
+
+
