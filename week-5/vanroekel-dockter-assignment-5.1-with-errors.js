f6d65670ffaa1b062fa9bf24b305f384ffa8922c
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-29 14:48:19 -0500

    vanroekel-dockter-assignment-5.1-errors-fixed.js

---
+++ week-5/vanroekel-dockter-assignment-5.1-with-errors.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  Assignment 5.1
+; Author: Brittany Dockter 
+; Date:   26 June 2019
+; Modified by: Faye Van Roekel
+; Description: Creating a function using maps.
+;===========================================
+*/
+
+
+/* --- Expected Output ---
+Array(5) [7, 9, 11, 13, 15]
+*/
+
+// Begin Program
+
+var array1 = [2, 4, 6, 8, 10]; //quotes are not needed around numbers
+var map1 = array1.map((x => 
+    {return x + 5})) //change map1 to x to declare the variable and take out x= and take out map
+
+
+// Output
+console.log (map1) //only had map and changed it to map1
+
+
+//end program')'
